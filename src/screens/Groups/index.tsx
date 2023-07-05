afe131782adc87896091fb9f6c1c08696a9acebc
Alehandro Queiroz <alehandroqueiroz.13@gmail.com>
2023-07-05 07:56:03 -0300

    primeiro commit

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+import { Container, Title } from './style';
+
+export function Groups() {
+  return (
+    <Container>
+      <Title>
+          Groups
+      </Title>
+    </Container>
+    
+  );
+}
